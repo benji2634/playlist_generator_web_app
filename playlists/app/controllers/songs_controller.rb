d1492c4ddabeb4086a68debddc99228a085cb78e
Tom Benham <benji26@hotmail.co.uk>
2017-01-23 22:23:41 +0000

    added further methods to songs controller

--- playlists/app/controllers/songs_controller.rb
+++ playlists/app/controllers/songs_controller.rb
@@ -5,15 +5,24 @@ class SongsController < ApplicationController
     render :json => songs
   end
 
+  def show
+    song = Song.find(params[:id])
+    render :json => song
+  end
+
   def create
     song = Song.create( song_params )
     render json: song, status: :created
   end
 
-  def show
-   song = Song.find(params[:id])
-   render :json => song
-  end
+ def update
+   song = Song.update(song_params)
+   render json: song, status: :created
+ end
+
+ def delete
+   Song.destroy(params[:id])
+ end
 
   private
   def song_params
